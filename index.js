e66528250b47d3d1ea157971ca537f1b7a7bc2cf
Tarun Singh <newtarunsh@gmail.com>
2025-02-20 11:03:01 +0000

    basic setup for react application is done

---
+++ index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+import App from "./Main Files/App";
+
+const root =ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>)
