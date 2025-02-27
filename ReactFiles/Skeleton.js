351dc169b192830541e98d48479466cd1724f5e2
Tarun Singh <tarunsingh0107@gmail.com>
2025-02-27 10:57:19 +0000

    added outlet in reactrouter

--- ReactFiles/Skeleton.js
+++ ReactFiles/Skeleton.js
@@ -1,11 +1,13 @@
 import Header from "./Skeleton/Header";
 import Footer from "./Footer";
 import Body from "./Skeleton/Body";
+import { Outlet } from "react-router-dom";
 const Skeleton = () => {
   return (
     <>
       <Header />
       <Body />
+      <Outlet />
       <Footer />
     </>
   );
